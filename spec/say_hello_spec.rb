964a9019e9a486e892d1f02ace5e28d0c3ad7f8a
Geoffrey Ontiri <ontirischool@gmail.com>
2021-02-10 01:23:55 +0000

    Automatically backed up by Learn

--- spec/say_hello_spec.rb
+++ spec/say_hello_spec.rb
@@ -1,5 +1,5 @@
-def say_hello ("Kent Beck" name ="Ruby Programmer")
-  puts "Hello, #{name}"
+def say_hello ("Hello Kent Beck" name ="Hello Ruby Programmer")
+  puts name
 end
 
 require_relative './spec_helper'
